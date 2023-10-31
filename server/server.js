7c19f119b49ad1db874789dce6748efb03ff3423
ShazebA <sashiqu@uwo.ca>
2023-10-31 16:13:34 -0400

    Added add superhero power search by ID endpoint

--- server/server.js
+++ server/server.js
@@ -24,8 +24,22 @@ app.get('/superhero/:id', (req, res) => {
     return res.json(hero);
 });
 
+app.get('/superhero/:id/powers', (req, res) => {
+    // Read the superhero_powers.json file
+    const powersList = JSON.parse(fs.readFileSync('superhero_powers.json', 'utf-8'));
+    
+    // Find the powers by hero name (assuming hero names are unique)
+    const powers = powersList.find(p => p.hero_names === req.params.id);
+    
+    if (!powers) {
+        return res.status(404).json({ error: "Powers not found for the superhero" });
+    }
+    
+    return res.json(powers);
+});
+
 
 
 app.listen(PORT, () => {
-    console.log(`Superhero app listening on port ${port}!`);
+    console.log(`Superhero app listening on port ${PORT}!`);
 })
