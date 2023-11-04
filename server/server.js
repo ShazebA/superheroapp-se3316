b678c2f60f4494e8168b9a85315a610c767701f1
ShazebA <sashiqu@uwo.ca>
2023-11-04 17:23:44 -0400

    Added sorting functionality to list results

--- server/server.js
+++ server/server.js
@@ -1,42 +1,87 @@
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+const clientFolderPath = path.join(__dirname, '..', 'client');
+app.use(express.static("C:/University/Year 3/SE3316/se3316-sashiqu-lab3/client"));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello, Superhero API!');
-});
+// app.get('/', (req, res) => { 
+//     res.sendFile("C:/University/Year 3/SE3316/se3316-sashiqu-lab3/client/index.html");
+// });
 
-app.get('/superhero/:id', (req, res) => {
+
+// app.get('/superhero/:id', (req, res) => {
+//     // Read the superhero_info.json file
+//     const superheroes = JSON.parse(fs.readFileSync('superhero_info.json', 'utf-8'));
+
+//     // Find the superhero by ID
+//     const hero = superheroes.find(h => h.id === parseInt(req.params.id));
+    
+//     if (!hero) {
+//         return res.status(404).json({ error: "Superhero not found" });
+//     }
+    
+//     return res.json(hero);
+// });
+
+// app.get('/superhero/:id/powers', (req, res) => {
+//     // Read the superhero_info.json file to get the superhero's name by ID
+//     const superheroes = JSON.parse(fs.readFileSync('superhero_info.json', 'utf-8'));
+//     const hero = superheroes.find(h => h.id === parseInt(req.params.id));
+
+//     if (!hero) {
+//         return res.status(404).json({ error: "Superhero not found" });
+//     }
+
+//     // Now, read the superhero_powers.json file to get the powers for the superhero's name
+//     const powersList = JSON.parse(fs.readFileSync('superhero_powers.json', 'utf-8'));
+    
+//     // Find the powers by hero name (assuming hero names are unique)
+//     const powers = powersList.find(p => p.hero_names === hero.name);
+    
+//     if (!powers) {
+//         return res.status(404).json({ error: "Powers not found for the superhero" });
+//     }
+    
+//     return res.json(powers);
+// });
+
+app.get('/superhero/search', (req, res) => {
     // Read the superhero_info.json file
     const superheroes = JSON.parse(fs.readFileSync('superhero_info.json', 'utf-8'));
-    
-    // Find the superhero by ID
-    const hero = superheroes.find(h => h.id === parseInt(req.params.id));
-    
-    if (!hero) {
-        return res.status(404).json({ error: "Superhero not found" });
+
+    // Extract query parameters and convert to lowercase for case-insensitive search
+    const nameQuery = req.query.name ? req.query.name.toLowerCase() : null;
+    const raceQuery = req.query.race ? req.query.race.toLowerCase() : null;
+    const publisherQuery = req.query.publisher ? req.query.publisher.toLowerCase() : null;
+
+    // Check if at least one query parameter is provided
+    if (!nameQuery && !raceQuery && !publisherQuery) {
+        return res.status(400).json({ error: "Provide at least one search parameter (name, race, or publisher)" });
     }
-    
-    return res.json(hero);
+
+    // Filter superheroes based on the provided query parameters
+    const matchedHeroes = superheroes.filter(hero => {
+        const matchesName = nameQuery ? hero.name.toLowerCase().includes(nameQuery) : true;
+        const matchesRace = raceQuery ? hero.Race.toLowerCase().includes(raceQuery) : true;
+        const matchesPublisher = publisherQuery ? hero.Publisher.toLowerCase().includes(publisherQuery) : true;
+
+        return matchesName && matchesRace && matchesPublisher;
+    });
+
+    // If no matches are found, return an appropriate response
+    if (matchedHeroes.length === 0) {
+        return res.status(404).json({ error: "No superheroes found matching the given criteria" });
+    }
+
+    return res.json(matchedHeroes);
 });
 
-app.get('/superhero/:id/powers', (req, res) => {
-    // Read the superhero_powers.json file
-    const powersList = JSON.parse(fs.readFileSync('superhero_powers.json', 'utf-8'));
-    
-    // Find the powers by hero name (assuming hero names are unique)
-    const powers = powersList.find(p => p.hero_names === req.params.id);
-    
-    if (!powers) {
-        return res.status(404).json({ error: "Powers not found for the superhero" });
-    }
-    
-    return res.json(powers);
-});
 
 app.get('/publishers', (req, res) => {
     const superheroes = JSON.parse(fs.readFileSync('superhero_info.json', 'utf-8'));
@@ -44,98 +89,135 @@ app.get('/publishers', (req, res) => {
     res.json(publishers);
 });
 
-app.get('/search', (req, res) => {
-    const { field, pattern, n } = req.query;
-
-    const superheroes = JSON.parse(fs.readFileSync('superhero_info.json', 'utf-8'));
-    const matchedHeroes = superheroes
-        .filter(hero => hero[field] && hero[field].toLowerCase().includes(pattern.toLowerCase()))
-        .slice(0, n ? parseInt(n) : undefined)
-        .map(hero => hero.id);
-
-    if (matchedHeroes.length === 0) {
-        return res.status(404).json({ error: "No superheroes found" });
-    }
-
-    res.json(matchedHeroes);
-});
-
 app.post('/list', (req, res) => {
     const { name } = req.body;
 
-    let lists = JSON.parse(fs.readFileSync('lists.json', 'utf-8'));
+    let lists = JSON.parse(fs.readFileSync('listsDatabase.json', 'utf-8'));
     if (lists[name]) {
         return res.status(400).json({ error: "List name already exists" });
     }
 
     lists[name] = [];
-    fs.writeFileSync('lists.json', JSON.stringify(lists, null, 2));
+    fs.writeFileSync('listsDatabase.json', JSON.stringify(lists, null, 2));
 
     res.json({ message: "List created successfully" });
 });
+
+app.get('/lists', (req, res) => {
+    try {
+        let lists = JSON.parse(fs.readFileSync('listsDatabase.json', 'utf-8'));
+        // Extract only the list names (which are the keys in the JSON object)
+        let listNames = Object.keys(lists);
+        res.json(listNames);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
 
 app.put('/list/:name', (req, res) => {
     const { name } = req.params;
     const { ids } = req.body;
 
-    let lists = JSON.parse(fs.readFileSync('lists.json', 'utf-8'));
+    let lists = JSON.parse(fs.readFileSync('listsDatabase.json', 'utf-8'));
     if (!lists[name]) {
         return res.status(404).json({ error: "List name does not exist" });
     }
 
-    lists[name] = ids;
-    fs.writeFileSync('lists.json', JSON.stringify(lists, null, 2));
+    if (!Array.isArray(ids)) {
+        return res.status(400).json({ error: "IDs should be provided as an array" });
+    }
 
+    // Convert the IDs from strings to numbers
+    const idNumbers = ids.map(Number);
+
+    // Append the IDs to the list, ensuring uniqueness
+    lists[name] = idNumbers;
+
+    fs.writeFileSync('listsDatabase.json', JSON.stringify(lists, null, 2));
     res.json({ message: "List updated successfully" });
 });
 
-app.get('/list/:name', (req, res) => {
-    const { name } = req.params;
 
-    const lists = JSON.parse(fs.readFileSync('lists.json', 'utf-8'));
-    if (!lists[name]) {
-        return res.status(404).json({ error: "List name does not exist" });
-    }
 
-    res.json(lists[name]);
-});
+// app.get('/list/:name', (req, res) => {
+//     const { name } = req.params;
+
+//     const lists = JSON.parse(fs.readFileSync('listsDatabase.json', 'utf-8'));
+//     if (!lists[name]) {
+//         return res.status(404).json({ error: "List name does not exist" });
+//     }
+
+//     res.json(lists[name]);
+// });
 
 app.delete('/list/:name', (req, res) => {
     const { name } = req.params;
 
-    let lists = JSON.parse(fs.readFileSync('lists.json', 'utf-8'));
+    let lists = JSON.parse(fs.readFileSync('listsDatabase.json', 'utf-8'));
     if (!lists[name]) {
         return res.status(404).json({ error: "List name does not exist" });
     }
 
     delete lists[name];
-    fs.writeFileSync('lists.json', JSON.stringify(lists, null, 2));
+    fs.writeFileSync('listsDatabase.json', JSON.stringify(lists, null, 2));
 
     res.json({ message: "List deleted successfully" });
 });
 
-app.get('/list/:name/details', (req, res) => {
+app.get('/list/details/:name', (req, res) => {
     const { name } = req.params;
+    const sortBy = req.query.sortBy || 'name'; // Default sorting by name
 
-    const lists = JSON.parse(fs.readFileSync('lists.json', 'utf-8'));
+    let lists = JSON.parse(fs.readFileSync('listsDatabase.json', 'utf-8'));
     if (!lists[name]) {
         return res.status(404).json({ error: "List name does not exist" });
     }
 
+    const heroIds = lists[name].map(Number);
     const superheroes = JSON.parse(fs.readFileSync('superhero_info.json', 'utf-8'));
-    const powersList = JSON.parse(fs.readFileSync('superhero_powers.json', 'utf-8'));
+    const powersData = JSON.parse(fs.readFileSync('superhero_powers.json', 'utf-8'));
 
-    const details = lists[name].map(id => {
-        const hero = superheroes.find(h => h.id === id);
-        const powers = powersList.find(p => p.hero_names === hero.name);
-        return { hero, powers };
+    let heroesInList = superheroes.filter(hero => heroIds.includes(hero.id)).map(hero => {
+        const heroPowers = powersData.find(p => p.hero_names === hero.name);
+        hero.powers = heroPowers ? Object.keys(heroPowers).filter(power => heroPowers[power] === "True") : [];
+        return hero;
     });
 
-    res.json(details);
+    // Sorting logic
+    heroesInList.sort((a, b) => {
+        if (sortBy === 'power') {
+            return b.powers.length - a.powers.length;
+        }
+        if (a[sortBy] < b[sortBy]) return -1;
+        if (a[sortBy] > b[sortBy]) return 1;
+        return 0;
+    });
+
+    res.json(heroesInList);
 });
+
+
+app.get('/list/ids/:name', (req, res) => {
+    const { name } = req.params;
+
+    let lists = JSON.parse(fs.readFileSync('listsDatabase.json', 'utf-8'));
+
+    if (!lists[name]) {
+        return res.status(404).json({ error: "List name does not exist" });
+    }
+
+    const heroIds = lists[name].map(Number); // Convert string IDs to numbers
+
+    res.json(heroIds);
+});
+
+
 
 
 
 app.listen(PORT, () => {
     console.log(`Superhero app listening on port ${PORT}!`);
 })
+
